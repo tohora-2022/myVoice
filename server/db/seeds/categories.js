5c449c51d7df690f7d9e450ef630c2b35e1061c2
Carol <93018447+carol-vsilva@users.noreply.github.com>
2022-02-23 21:21:36 +1300

    Added quick as a category

--- server/db/seeds/categories.js
+++ server/db/seeds/categories.js
@@ -2,13 +2,14 @@ exports.seed = function (knex) {
   return knex('categories').del()
     .then(function () {
       return knex('categories').insert([
-        { id: 1, category: 'actions' },
-        { id: 2, category: 'describe' },
-        { id: 3, category: 'help' },
-        { id: 4, category: 'people' },
-        { id: 5, category: 'food' },
-        { id: 6, category: 'activities' },
-        { id: 7, category: 'clothes' }
+        { id: 1, category: 'quick' },
+        { id: 2, category: 'actions' },
+        { id: 3, category: 'describe' },
+        { id: 4, category: 'help' },
+        { id: 5, category: 'people' },
+        { id: 6, category: 'food' },
+        { id: 7, category: 'activities' },
+        { id: 8, category: 'clothes' }
       ])
     })
 }
