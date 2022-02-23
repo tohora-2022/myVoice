06b75176b1eb45a659f043417d49c09bee709872
Carol <93018447+carol-vsilva@users.noreply.github.com>
2022-02-24 10:38:11 +1300

    Image reader config on webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/
