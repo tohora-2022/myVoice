e94026a08fbec9c006d249af28d46a8645184f38
Carol <93018447+carol-vsilva@users.noreply.github.com>
2022-02-24 10:32:15 +1300

    Words added on db - image path needs fix

--- server/db/seeds/categories.js
+++ server/db/seeds/categories.js
@@ -2,14 +2,15 @@ exports.seed = function (knex) {
   return knex('categories').del()
     .then(function () {
       return knex('categories').insert([
-        { id: 1, category: 'quick' },
-        { id: 2, category: 'actions' },
-        { id: 3, category: 'describe' },
-        { id: 4, category: 'help' },
-        { id: 5, category: 'people' },
-        { id: 6, category: 'food' },
-        { id: 7, category: 'activities' },
-        { id: 8, category: 'clothes' }
+        { id: 101, category: 'quick' },
+        { id: 102, category: 'actions' },
+        { id: 103, category: 'describe' },
+        { id: 104, category: 'help' },
+        { id: 105, category: 'people' },
+        { id: 106, category: 'food' },
+        { id: 107, category: 'activities' },
+        { id: 108, category: 'clothes' },
+        { id: 109, category: 'homepage' }
       ])
     })
 }
