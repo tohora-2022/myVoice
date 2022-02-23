1f642efedd83fd22a5e778b76a81180ec947a5a7
ben-hoggan <hoggan.ben@gmail.com>
2022-02-24 11:07:06 +1300

    Added set category redux to category component.

--- client/components/Category.jsx
+++ client/components/Category.jsx
@@ -1,33 +1,30 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { setCategory } from '../actions/category'
 
 function Category ({ name, image, items }) {
   const [itemsToDisplay, setItemsToDisplay] = useState()
-  console.log(items.slice(0, 5))
-  // Dynamic Dimension, to be used for other display sizes.
-  const [screenSize, setScreenSize] = useState({
-    dynamicWidth: window.innerWidth,
-    dynamicHeight: window.innerHeight
-  })
-  const setDimension = () => {
-    setScreenSize({
-      dynamicWidth: window.innerWidth,
-      dynamicHeight: window.innerHeight
-    })
-  }
+  const dispatch = useDispatch()
+  // Dynamic Dimensions, to be used later for other display sizes.
+  const [screenSize, setScreenSize] = useState({ dynamicWidth: window.innerWidth, dynamicHeight: window.innerHeight })
+  const setDimension = () => { setScreenSize({ dynamicWidth: window.innerWidth, dynamicHeight: window.innerHeight }) }
   useEffect(() => {
     window.addEventListener('resize', setDimension)
     setItemsToDisplay(1)
-    return () => {
-      window.removeEventListener('resize', setDimension)
-    }
+    return () => { window.removeEventListener('resize', setDimension) }
   }, [screenSize])
 
   useEffect(() => {
     setItemsToDisplay(items.slice(0, 5))
   }, [items])
 
+  const categoryClickHandler = () => {
+    dispatch(setCategory(name))
+  }
+
   return (
-    <div className='category'>
+    <div className='category' onClick={categoryClickHandler}>
       <div className='categoryName'>
         <img className='categoryImage' src={image}/>
         {name}
