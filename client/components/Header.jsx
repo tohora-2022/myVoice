6a8668a9b49c5e810caeb0248fe839520a088433
Paige-Moore <paigedmoore@outlook.com>
2022-02-26 10:36:22 +1300

    top bar and side bar styles

---
+++ client/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Image,
+  Box,
+} from "@chakra-ui/react";
+
+const Header = () => {
+ 
+  return (
+    <Box
+      as="nav"
+      height='120px'
+      padding={2}
+      align="center"
+      wrap="wrap"
+      bg="#21ad09"
+      color="white"
+    >
+      
+      <Box align="center">
+    
+      <Image
+      borderRadius={60}
+       boxSize='100px'
+       width='110px'
+       boxShadow='dark-lg' rounded='md' bg='white'align="center"
+  src='/images/MyVoiceLogo.png'
+  alt='My Voice'
+/>
+      </Box>    
+    </Box>
+  );
+};
+
+export default Header;
