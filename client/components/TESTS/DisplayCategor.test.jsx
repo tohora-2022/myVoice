fcd9eca49054d72f286a04ee68ca0318af5429e8
Carol <93018447+carol-vsilva@users.noreply.github.com>
2022-02-25 13:11:07 +1300

    Test for Display Category component

--- client/components/TESTS/DisplayCategor.test.jsx
+++ client/components/TESTS/DisplayCategor.test.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { screen, render } from '@testing-library/react'
+import { screen, render, waitFor } from '@testing-library/react'
 
 import DisplayCategory from '../DisplayCategory'
-import { getItems } from '../apis/api'
+import { getItems } from '../../apis/api'
 
-jest.mock('../apis/api')
+jest.mock('../../apis/api')
 
 describe('<DisplayCategory />', () => {
-  const fakeCategory = {
+  const fakeStore = {
+    subscribe: jest.fn(),
     getState: jest.fn(),
     dispatch: jest.fn()
   }
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+    fakeStore.getState.mockReturnValue([])
+  })
+
+  it('displays items from redux state', () => {
+    getItems.mockReturnValue(Promise.resolve([{ itemId: 1, word: 'play', image: 'images/actions/play.png' }, { itemId: 2, word: 'brush teeth', image: 'images/actions/brush_teeth.png' }]))
+    fakeStore.getState.mockReturnValue({ category: 101 })
+    render(<Provider store={fakeStore}><DisplayCategory/></Provider>)
+    return waitFor(() => {
+      expect(getItems).toHaveBeenCalled()
+    })
+      .then(() => {
+        const listItems = screen.getAllByRole('img')
+        const description = screen.getByText('play')
+        console.log(description)
+        expect(listItems).toHaveLength(2)
+        expect(listItems[0].alt).toBe('play')
+        expect(listItems[1].alt).toBe('brush teeth')
+        expect(description).toContain('p')
+        return null
+      })
+  })
 })
