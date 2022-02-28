71c9a95aca196cd9c7bdf45d0d76367ccf1073a9
Carol <93018447+carol-vsilva@users.noreply.github.com>
2022-02-28 16:52:11 +1300

    Quick display should be different

--- client/components/Categories.jsx
+++ client/components/Categories.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 import Category from './Category'
+import QuickRows from './QuickRows'
 
 function Categories () {
   const categories = useSelector(state => state.categories)
 
+  // function splitInHalf (arr) {
+  //   const half = Math.ceil(arr.length / 2)
+  //   const firstHalf = arr.slice(0, half)
+  //   const secondHalf = arr.slice(-half)
+
+  //   return [firstHalf, secondHalf]
+  // }
+
   return (
     <div>
       {categories?.map(each => {
-        return <Category key={each.id} name={each.category} id={each.id} image={each.image}/>
+        each.category === 'quick' ? <QuickRows key={each.id} name={each.category}/> : <Category key={each.id} name={each.category} id={each.id} image={each.image}/>
       })}
     </div>
   )
--- client/components/QuickRows.jsx
+++ client/components/QuickRows.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { useBreakpointValue, HStack, Image, Center } from '@chakra-ui/react'
+import { useBreakpointValue, HStack, Center } from '@chakra-ui/react'
 
 import Item from './Item'
 
-function Category ({ name, id, image }) {
+export default function QuickRows ({ name }) {
   const items = useSelector(state => state.items[name])
-  // const imageSize = useSelector(state => state.zoom)
 
   const numToShow = useBreakpointValue({
     base: 5,
@@ -24,17 +22,11 @@ function Category ({ name, id, image }) {
     <>
       <HStack spacing={6} mx={3} px={2} borderRadius={5} mb={1} h={rowHeight} border='2px' borderColor='blue.600'>
         <Center height="full">
-          <Link to={`/${name}`}>
-            <Image src={image} alt={name} maxWidth="130px" height="auto"/>
-          </Link>
+          {items?.map(item => {
+            return <Item key={item.itemId} item={item} />
+          })}
         </Center>
-
-        {items?.slice(0, numToShow).map(item => {
-          return <Item key={item.itemId} item={item} />
-        })}
       </HStack>
     </>
   )
 }
-
-export default Category
